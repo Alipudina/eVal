515c1e994fa2eb47e9a64e8468a1a5a5818b280c
MeirOverferst <meiroverferst@gmail.com>
2019-05-25 11:17:31 +0200

    added basic routes and structure

--- browser/src/App.js
+++ browser/src/App.js
@@ -1,38 +1,62 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.scss';
+import Footer from './components/footer'
+import Header from './components/header'
+import Body from './components/body'
 
-class App extends Component {
+import { BrowserRouter } from 'react-router-dom';
 
-  render() {
-    return (
-      <div className="start-page">
-        <div className="header">
-          <div>Logo</div>
-          <div className="login-container">
-            <button>login</button>
-            <button>Not a user</button>
-          </div>
-        </div>
-
-        <div className="info-container">
-          <p>
-            eVAL is an easy, convenient and intuitive tool to create your own evaluation test.
-            With eVAL you can create your own questions and choose the type of answer that best fits
-            your needs: Yes/No, Multiple Choice or Scrambled Text.
-            Store different set of tests to apply whenever you need.
-            Send the link of a test to anyone, by e-mail, and receive the results once they’ve finished.
-            eVAL, as simple as that!
-          </p>
-        </div>
-
-        <div className="footer">
-          <button>About US</button>
-          <button>Info</button>
-          <button>another buuton</button>
-        </div>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Body />
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
+
+
+
+
+
+// import React, { Component } from 'react';
+// import './App.scss';
+
+// class App extends Component {
+
+//   render() {
+//     return (
+//       <div className="start-page">
+//         <div className="header">
+//           <div>Logo</div>
+//           <div className="login-container">
+//             <button>login</button>
+//             <button>Not a user</button>
+//           </div>
+//         </div>
+
+//         <div className="info-container">
+//           <p>
+//             eVAL is an easy, convenient and intuitive tool to create your own evaluation test.
+//             With eVAL you can create your own questions and choose the type of answer that best fits
+//             your needs: Yes/No, Multiple Choice or Scrambled Text.
+//             Store different set of tests to apply whenever you need.
+//             Send the link of a test to anyone, by e-mail, and receive the results once they’ve finished.
+//             eVAL, as simple as that!
+//           </p>
+//         </div>
+
+//         <div className="footer">
+//           <button>About US</button>
+//           <button>Info</button>
+//           <button>another buuton</button>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
---
+++ browser/src/components/login.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default class LoginPgae extends Component {
+  render() {
+    return (
+      <>
+        <div className="login">
+          <h1>login page</h1>
+          <form className="loginForm">
+            <label>userName</label>
+            <input placeholder="userName" />
+            <label>Password</label>
+            <input placeholder="password" />
+            <button>Submit</button>
+          </form>
+          <form className="createAccountForm" >
+            <h1>Not a user ? Create account</h1>
+            <label>Name</label>
+            <input placeholder="Name" />
+            <label>Email</label>
+            <input placeholder="Email" />
+            <label>userName</label>
+            <input placeholder="userName" />
+            <label>Password</label>
+            <input placeholder="password" />
+            <button>Submit</button>
+          </form>
+          <NavLink to="/creation" className="logbtn">CreationTempLink</NavLink>
+        </div>
+      </>
+    )
+  }
+}
