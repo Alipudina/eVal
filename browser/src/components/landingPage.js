586484ea130e21cc64532639aca2ae5e1010b934
MeirOverferst <meiroverferst@gmail.com>
2019-06-18 16:35:16 +0200

    landing move element

--- browser/src/components/landingPage.js
+++ browser/src/components/landingPage.js
@@ -9,8 +9,8 @@ export default class LandingPage extends Component {
                 <NavLink to="/login" className="  btn btn-primary float-right">LogIn/SignUp</NavLink>
                 <div className="landingPage Container mt-5">
                     <h1> LandingPage</h1>
-                    <div className="p-5 p-lg-5">
-                        <p className="Card">
+                    <div className="">
+                        <p className="Card w-50 mx-auto my-5">
                             eVAL is an easy, convenient and intuitive tool to create your own evaluation test.
                             With eVAL you can create your own questions and choose the type of answer that best fits
                             your needs: Yes/No, Multiple Choice or Scrambled Text.             Store different set of tests to apply whenever you need.
